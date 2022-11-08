d85c4094d51c67e58bf69bb9aa04c28037f900c9
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-08 19:13:27 +0430

    Adding test for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -37,3 +37,21 @@ describe 'reverse a word - ' do
     expect(solver.reverse('hello')).to eq('olleh')
   end
 end
+
+describe 'famous fizzbuzz - ' do
+  it "should return 'fizz' if int is divisible by 3" do
+    expect(solver.fizzbuzz(3)).to eq('fizz')
+  end
+
+  it "should return 'buzz' if int is divisible by 5" do
+    expect(solver.fizzbuzz(5)).to eq('buzz')
+  end
+
+  it "should return 'fizzbuzz' if int is divisible by 3 and 5" do
+    expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+  end
+
+  it "should return 'int' if int is not divisible by either 3 or 5" do
+    expect(solver.fizzbuzz(7)).to eq('7')
+  end
+end
