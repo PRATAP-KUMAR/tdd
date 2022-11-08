31b09136f33e3c17d31e61c8ab2facd41584ae02
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-08 19:12:58 +0430

    Add method for fizzbuzz function

--- solver.rb
+++ solver.rb
@@ -3,5 +3,15 @@ class Solver
 
   def reverse(word); end
 
-  def fizzbuzz(int); end
+  def fizzbuzz(int)
+    if int.modulo(3).zero? && int.modulo(5).zero?
+      'fizzbuzz'
+    elsif int.modulo(3).zero?
+      'fizz'
+    elsif int.modulo(5).zero?
+      'buzz'
+    else
+      int.to_s
+    end
+  end
 end
